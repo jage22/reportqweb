d84f36062a83763109ff9d9558ff7a2de3d22de0
Jesus Alan Guzman Escalante <aguzman@vauxoo.com>
2014-08-25 18:17:41 -0500

    [ADD]module

---
+++ reportqweb/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+{
+	'name' : "Test report",
+	'category' : "test",
+	'version' : "1.0",
+	'depends' : ['base'],
+	'author' : "J.A. Guzman",
+	'installable' :True,
+	'auto_install' : False,
+	'description' : """\
+This module generate an report of try:
+==================================
+	- generate reports of facturation """,
+	
+	
+	
+	'data' : [
+		#'view/sale.xml',
+		'report/report.xml',
+		'report/reporte.xml',
+	],
+}
+
+
+
+
+
+
